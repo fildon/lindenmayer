6edd08ffa577a90e0f153cfc25d9572914c99b26
Rupert McKay <maxfmckay@gmail.com>
2019-07-03 14:54:10 +0100

    Full breadfirst painting implementation

--- main.js
+++ main.js
@@ -1,58 +1,89 @@
-function generateBranchSystem(branchingFactor) {
-    branches = [];
-    for (i = 0; i < branchingFactor; ++i) {
-        branches.push(
-            {
-                baseLength: 40 * (Math.random() * 0.4 + 0.6),
-                scaleChange: Math.random() * 0.4 / ((i + 1) * (i + 1)) + 0.55,
-                angleChange: 2 * Math.PI * Math.random()
-            });
+function BranchGroup(canvasContext) {
+    this.branches = [new Branch(canvasContext), new Branch(canvasContext)];
+    this.draw = (startPoint, rotation, depth) => {
+        this.branches.forEach(branch => {
+            branch.draw(startPoint, rotation, depth);
+        })
+    };
+}
+
+function Branch(canvasContext) {
+    this.canvasContext = canvasContext;
+    this.baseLength = 90 * (Math.random() * 0.4 + 0.6);
+    this.angle = 2 * Math.PI * Math.random();
+    this.getEndpoint = (startPoint, rotation, depth) => {
+        const length = this.baseLength * (Math.pow(0.9, depth));
+        return startPoint.translate(length, rotation + this.angle);
+    };
+    this.draw = (startPoint, rotation, depth) => {
+        const endPoint = this.getEndpoint(startPoint, rotation, depth)
+        canvasContext.strokeStyle = "hsl(" + (40 * depth) + ", 100%, 50%)";
+        canvasContext.lineWidth = 4 / (depth + 2);
+        canvasContext.beginPath();
+        canvasContext.moveTo(startPoint.x, startPoint.y);
+        canvasContext.lineTo(endPoint.x, endPoint.y);
+        canvasContext.stroke();
+    };
+}
+
+function Point(x, y) {
+    this.x = x;
+    this.y = y;
+    this.translate = (length, rotation) => {
+        return new Point(
+            this.x + length * Math.cos(rotation),
+            this.y + length * Math.sin(rotation)
+        );
     }
-    return branches;
 }
 
-function draw_line(x, y, length, angle, depth) {
-    let ctx = can.getContext("2d");
-    ctx.strokeStyle = "hsl(" + (40 * depth) + ", 100%, 50%)";
-    ctx.lineWidth = depth / 6;
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(x + length * Math.cos(angle), y + length * Math.sin(angle));
-    ctx.stroke();
-}
-
-function draw_lsystem(lsystem, x, y, depth = 12, scale = 4, angle = 0) {
-    if (depth <= 0)
-        return;
-    if (x < 0 || y < 0 || x > width || y > height)
-        return;
-    while (branchesToDraw[branchesToDraw.length - 1] < lsystem.length) {
-        branch = lsystem[branchesToDraw[branchesToDraw.length - 1]];
-        scaledLength = branch.baseLength * scale;
-        newAngle = angle + branch.angleChange;
-        branchesToDraw.push(0);
-        if (scaledLength > 2) {
-            draw_line(x, y, scaledLength, newAngle, depth);
-            draw_lsystem(
-                lsystem,
-                x + Math.cos(newAngle) * scaledLength,
-                y + Math.sin(newAngle) * scaledLength,
-                depth - 1,
-                scale * branch.scaleChange,
-                newAngle
-            );
+function PaintingTask(branchGroup, startPoint, rotation, depth) {
+    this.branchGroup = branchGroup;
+    this.startPoint = startPoint;
+    this.rotation = rotation;
+    this.depth = depth;
+    this.draw = () => {
+        this.branchGroup.draw(this.startPoint, this.rotation, this.depth);
+    };
+    this.getChildren = () => {
+        if (this.depth > 10) {
+            return [];
         }
-        branchesToDraw.pop();
-        ++branchesToDraw[branchesToDraw.length-1]
-    }
+        const children = [];
+        this.branchGroup.branches.forEach(branch => {
+            children.push(new PaintingTask(
+                this.branchGroup,
+                branch.getEndpoint(this.startPoint, this.rotation, this.depth),
+                this.rotation + branch.angle,
+                this.depth + 1)
+            );
+        });
+        return children;
+    };
 }
 
-var branchesToDraw = [0];
+function breadthFirstPaint() {
+    const canvas = document.getElementById("canvas");
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    const canvasContext = canvas.getContext("2d");
+    let paintingQueue = [
+        new PaintingTask(
+            new BranchGroup(canvasContext),
+            new Point(canvas.width / 2, canvas.height / 2),
+            0,
+            0
+        )
+    ];
+
+    while (paintingQueue.length > 0) {
+        // shift lets us use an array like a queue
+        const currentTask = paintingQueue.shift();
+        currentTask.draw();
+        paintingQueue = paintingQueue.concat(currentTask.getChildren());
+    }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-    width = window.innerWidth;
-    height = window.innerHeight;
-    can.width = width;
-    can.height = height;
-    draw_lsystem(generateBranchSystem(2), width / 2, height / 2);
+    breadthFirstPaint();
 }, false)
